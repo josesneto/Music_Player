3412b0d2302287d684d33b1290832c1f56b557de
josesneto <josesneto@users.noreply.github.com>
2020-07-08 21:27:11 -0300

    Inserção do botão next music, nova pasta para as músicas

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function pauseMusic() {
+var isPlaying = false;
+var musicArray = [
+    'medicine-remix.mp3',
+    'no-sleep.mp3'
+];
+var i = 0;
+function togglePlayPause() {
     player = document.getElementById("player");
-    isPlaying = false;
     player.onplaying = function () {
         isPlaying = true;
         console.log('playing');
@@ -12,9 +17,37 @@ function pauseMusic() {
     if (isPlaying) {
         player.pause();
         console.log('pause');
+        isPlaying = false;
+        toggleButtonText();
     }
     else {
         player.play();
         console.log('play');
+        isPlaying = true;
+        toggleButtonText();
     }
+}
+
+function toggleButtonText() {
+    button = document.getElementById("playPauseButton");
+    if (button.innerHTML == "PLAY") {
+        button.innerHTML = "PAUSE";
+    }
+    else {
+        button.innerHTML = "PLAY";
+    }
+}
+
+function nextMusic() {
+    playerBox = document.getElementById("playerBox");
+    music = 'musics/' + musicArray[i];
+    if (i < musicArray.length-1) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+    newPlayerBox = "<audio id='player' src='" + music + "' autoplay></audio>";
+    console.log(music);
+    playerBox.innerHTML = newPlayerBox;
 }
